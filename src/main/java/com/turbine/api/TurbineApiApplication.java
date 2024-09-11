4f5f701f27bb7184b3ee9ccf7fd404d3bd0573f9
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-09-11 14:54:32 +0900

    initial commit

---
+++ src/main/java/com/turbine/api/TurbineApiApplication.java
@@ -0,0 +1,13 @@
+package com.turbine.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbineApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turbine/api/TurbineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turbine.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
